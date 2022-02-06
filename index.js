8b32805763147678eb982adf00b94c79cf60ed17
Binaryleo <leomouracss@fastmail.com>
2022-02-05 23:30:59 -0300

    js file and responsiveness

---
+++ index.js
@@ -0,0 +1,24 @@
+const tabItems = document.querySelectorAll('.tab-item')//Tab item (click)
+const tabContentItems = document.querySelectorAll('.tab-content-item')//content to display and hide
+
+function selectItem(e) {
+  removeBorder()
+  removeShow();
+  //Add border to current tab content
+  this.classList.add('tab-border')
+  //Grab content item DOM element
+  console.log(this.id);
+ 
+  //add show class
+  const tabContentItems = document.querySelector(`#${this.id}-content`);
+  tabContentItems.classList.add('show');
+}
+// Remove show class from all content items
+function removeShow(){
+ tabContentItems.forEach(item => item.classList.remove('show'))
+}
+function removeBorder() {
+tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+//Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem))
